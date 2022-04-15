ca5ee86be4c6cb492075d34bfde8f8616d87a924
Tarlok-Kaur <103706363+Tarlok-Kaur@users.noreply.github.com>
2022-04-14 20:54:54 -0400

    Add files via upload

---
+++ source.js
@@ -0,0 +1,11 @@
+function display(){
+    document.getElementById('f1').innerHTML = document.myform;
+
+}
+
+function hiddenBox(){
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+} 
+
